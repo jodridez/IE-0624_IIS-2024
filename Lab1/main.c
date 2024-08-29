b1a39c81a040ada623fbfc49456244a22a597b53
Jonathan <jodridez.2.0@gmail.com>
2024-08-29 00:50:18 -0600

    Se comenta el codigo

--- Lab1/main.c
+++ Lab1/main.c
@@ -11,18 +11,14 @@ Carnet: B76490.
 Fecha: 28/8/2024.
 
 Descripcion:
+    Programa destinado a ser implementado en un microcontrolador pic12f683
+    controla una tombola digital que puede producir hasta 10 numeros aleatorios
+    antes de reiniciarse
 */
-
-
 #include <pic14/pic12f683.h>
 
-/*
-Configuracion:
-  _WDTE_OFF: Desactiva el watchdog Timer para evitar interrupciones inesperadas.
-*/
-typedef unsigned int word;
-word __at 0x2007 __CONFIG = (_WDTE_OFF & _MCLRE_OFF);
 
+// Definiciones de pines
 #define DATA_PIN   GP0 // Pin de datos (conectado a DS del 74HC595)
 #define CLOCK_PIN  GP4 // Pin del reloj de desplazamiento (conectado a SH_CP del 74HC595)
 #define LATCH_PIN  GP5 // Pin del reloj de almacenamiento (conectado a ST_CP del 74HC595)
@@ -30,7 +26,8 @@ word __at 0x2007 __CONFIG = (_WDTE_OFF & _MCLRE_OFF);
 #define DISPLAY2_PIN GP2 // Control del segundo display
 #define BUTTON_PIN GP3 // Pin del botón
 
-// Definición de los segmentos para los números 0-9
+// Declaacion de variables
+// Segmentos para los números 0-9
 const char segmentos[10] = {
     0b00111111, // 0 -> a,b,c,d,e,f
     0b00000110, // 1 -> b,c
@@ -44,8 +41,29 @@ const char segmentos[10] = {
     0b01101111  // 9 -> a,b,c,d,f,g
 };
 
+unsigned int revolviendo = 0;
+unsigned int resultado = 0;
+unsigned int numeroSorteo = 0;
+unsigned int posicion = 0;
+unsigned int numerosSorteados[10] = {0};
+unsigned int unidad = 0;
+unsigned int decena = 0;
 
+/*
+Configuracion:
+  _WDTE_OFF: Desactiva el watchdog Timer para evitar interrupciones inesperadas.
+  _MCLRE_OFF: Desactiva la función MCLR, permitiendo el uso de GP3 como pin I/O.
+*/
+typedef unsigned int word;
+word __at 0x2007 __CONFIG = (_WDTE_OFF & _MCLRE_OFF);
 
+///////////////////////////////////////////////////
+// Funciones
+/*
+    enviarDato: Se encarga del envio de datos
+    al 74HC595 para encender los segmentos
+    deseados
+*/
 void enviarDato(unsigned char dato) {
     for (int i = 0; i < 8; i++) {
         DATA_PIN = (dato & 0x80) >> 7; // Enviar el bit más significativo primero
@@ -55,6 +73,13 @@ void enviarDato(unsigned char dato) {
     }
 }
 
+/*
+    mostrarNumero: Se encarga de intercambiar el
+    encendido de las pantallas, asi como de 
+    enviarles el numero deseado
+    la velocidad de la permutacion da el efecto de 
+    tener 2 digitos con la dos pantallas encendidadas
+*/
 void mostrarNumero(unsigned char numero, char display) {
     if (display == 1) {
         DISPLAY1_PIN = 1; // Activar el primer display
@@ -75,6 +100,7 @@ void mostrarNumero(unsigned char numero, char display) {
 }
 
 
+// numeroYaSorteado: Verifica que el numero no haya salido ya
 int numeroYaSorteado(int numero, int numerosSorteados[]) {
     for (int i = 0; i < 11; i++) {
         if (numerosSorteados[i] == numero) {
@@ -92,7 +118,7 @@ void delay(unsigned int tiempo) {
         for(j = 0; j < 1275; j++);
 }
 
-
+// parpadeo99: parpadea 3 veces en la pantalla el numero 99
 void parpadeo99(){
     for (int i = 0; i < 3; i++)
     {
@@ -105,26 +131,14 @@ void parpadeo99(){
     }
 }
 
-unsigned int revolviendo = 0;
-unsigned int resultado = 0;
-unsigned int numeroSorteo = 0;
-unsigned int posicion = 0;
-unsigned int numerosSorteados[10] = {0};
-unsigned int unidad = 0;
-unsigned int decena = 0;
 
-
-
-
+////////////////////////////////////////////////////////////
 void main() {
     // Configuración de los pines como salida
     TRISIO = 0b00001000; // GP3 como entrada (botón), el resto como salidas
     ANSEL = 0x00; // Desactiva funciones analógicas
     CMCON0 = 0x07; // Desactiva comparadores
-
     GPIO = 0x00;   //Salidas inicialmente en 0
-
-
 
     while (1) {
         // Incrementar el número "revolviendo" constantemente
@@ -138,8 +152,6 @@ void main() {
                 resultado = revolviendo;
                 numerosSorteados[posicion] = resultado; 
                 posicion++;
-                // Calcular las decenas y unidades del número a mostrar
-
             }
 
             if (posicion==11)
