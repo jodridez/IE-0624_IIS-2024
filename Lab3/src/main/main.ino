c26bcb1a910ff3190bfe9899cac3c039a5dfc3f3
Jonathan <jodridez.2.0@gmail.com>
2024-10-05 21:59:29 -0600

    Se agregab fuentes DC, falta condicionar

--- Lab3/src/main/main.ino
+++ Lab3/src/main/main.ino
@@ -37,6 +37,10 @@ Descripcion:
 #define LED3_PIN 2 // Puerto digital para el led 3
 #define LED4_PIN 8 // Puerto digital para el led 4
 
+#define MODO_PIN 9 // Puerto digital para el switch de modo AC/DC
+#define COMUNICACION_PIN 10 // Puerto digital para la comunicacion serial
+/*
+
 /*
  Declaracion tensiones
 */
@@ -46,6 +50,11 @@ float V3; // Valor de la tension V3
 float V4; // Valor de la tension V4
 
 /*
+  Declaracion de estados de los switches
+*/
+ int modo;  // AC o DC
+/*
+
 Instancia de la pantalla LCD
 */
 PCD8544 lcd;
@@ -53,17 +62,27 @@ PCD8544 lcd;
 /////////////////////////////////////////
 //Funciones de configuracion
 /////////////////////////////////////////
+
+void setup_lcd(){
+  lcd.begin();
+  lcd.setContrast(60);
+
+}
 //Funcion para evitar mucho copy-paste. genera la estructura para inmprimir en pantalla
 void print_V( int modo, int line, String Vstring, float V){
   if(modo == 0){
+    //float Vrms = V / sqrt(2);
     lcd.setCursor(0, line);
     lcd.print(Vstring);
     lcd.print(V);
+    lcd.setCursor(995, line);
     lcd.print("Vrms");
-  } else {
+  }
+  if(modo == 1){
     lcd.setCursor(0, line);
     lcd.print(Vstring);
     lcd.print(V);
+    lcd.setCursor(995, line);
     lcd.print("V");
   }
 }
@@ -76,34 +95,49 @@ void pantalla_lcd(float V1, float V2, float V3, float V4, int modo) {
     lcd.print("AC");
     
     // [V1] Mostrar el valor de la tension V1
-    print_V(modo, 1, "V1: ", V1);
+    print_V(modo, 1, "V1:", V1);
     // [V2] Mostrar el valor de la tension V2
-    print_V(modo, 2, "V2: ", V2);
+    print_V(modo, 2, "V2:", V2);
     // [V3] Mostrar el valor de la tension V3
-    print_V(modo, 3, "V3: ", V3);
+    print_V(modo, 3, "V3:", V3);
     // [V4] Mostrar el valor de la tension V4
-    print_V(modo, 4, "V4: ", V4);
-  } else {
+    print_V(modo, 4, "V4:", V4);
+  } 
+  if(modo == 1){
     lcd.setCursor(0,0);
     lcd.print("DC");
 
     // [V1] Mostrar el valor de la tension V1
-    print_V(modo, 1, "V1: ", V1);
+    print_V(modo, 1, "V1:", V1);
     // [V2] Mostrar el valor de la tension V2
-    print_V(modo, 2, "V2: ", V2);
+    print_V(modo, 2, "V2:", V2);
     // [V3] Mostrar el valor de la tension V3
-    print_V(modo, 3, "V3: ", V3);
+    print_V(modo, 3, "V3:", V3);
     // [V4] Mostrar el valor de la tension V4
-    print_V(modo, 4, "V4: ", V4);
+    print_V(modo, 4, "V4:", V4);
   }
 }
 
+void limpiar_pantalla(bool limpiar){
+  if(!limpiar){
+    lcd.clear();
+  }
+  limpiar = true;
+}
+
 // Convierte el valor leido a tension
-float tension(int PIN)
-{ float valor_leido = analogRead(PIN);
+float tension(int PIN, int modo)
+{ 
+  float valor_leido = analogRead(PIN);
   float tension_condicionada = (valor_leido * 5) / 1023.0;
-  float tension = ((tension_condicionada - 1.54) / 3.45) * 48 - 24; 
-  return tension;
+  float tension = ((tension_condicionada) / 4.99) * 48 - 24; 
+  
+  if(modo == 0){
+    return tension; //AC
+  }
+  if(modo == 1){
+    return tension_condicionada; //DC
+  }
 }
 
 // Gestiona los LEDs
@@ -115,25 +149,38 @@ void leds(int LED_PIN, float V) {
   }
 }
 
+
+/////////////////////////////////////////
+//Funcion principal
+/////////////////////////////////////////
 void setup() {
   // put your setup code here, to run once:
-  lcd.begin();
+  setup_lcd();
+  // Configurar los pines de los LEDs
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
   pinMode(LED3_PIN, OUTPUT);
   pinMode(LED4_PIN, OUTPUT);
+
+  pinMode(MODO_PIN, INPUT);
+  pinMode(COMUNICACION_PIN, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Leer las tensiones
-  V1 = tension(V1_PIN);
-  V2 = 0;
-  V3 = 0;
-  V4 = 0;
+  if(digitalRead(MODO_PIN) == HIGH){
+    modo = 1; // Modo DC
+  } else {
+    modo = 0; // Modo AC
+  }
 
-  // Mostrar las tensiones en la pantalla
-  pantalla_lcd(V1, V2, V3, V4, 0);
+  // Leer las tensiones
+  V1 = tension(V1_PIN, modo);
+  V2 = tension(V2_PIN, modo);
+  V3 = tension(V3_PIN, modo);
+  V4 = tension(V4_PIN, modo);
+
+  pantalla_lcd(V1, V2, V3, V4, modo); // Modo DC
 
   // Gestionar los LEDs
   leds(LED1_PIN, V1);
